683a2f1897b7b7b4074a1f645b9a501261d564db
Willa Wang <willa@neo.com>
2014-12-07 11:43:05 -0500

    Refactored event_generator to return title and subtitle separately.

--- app/models/event_generator.rb
+++ app/models/event_generator.rb
@@ -5,17 +5,50 @@ class EventGenerator
   def self.fetch
     doc = Nokogiri::HTML(open("http://www.theskint.com"))
     today = doc.css("article").first
-    title = today.css("header .entry-title").text.strip
-    main_content = today.css(".entry-content p")
-    subtitle = main_content.first.css("u").text
-    events = main_content.css("p").drop(1)
+    EventGenerator.new(today)
+  end
 
-    events.map do |event|
-      if event.css("a").present?
-        { description: event.css("span").text,
-          link: event.css("a").attr("href").value
+  def as_json(options={})
+    {
+      date: title,
+      subtitle: subtitle,
+      events: events
+    }
+  end
+
+  def initialize(data)
+    @today = data
+  end
+
+  def title
+    @today.css("header .entry-title").text.strip
+  end
+
+  def main_content
+    @today.css(".entry-content p")
+  end
+
+  def subtitle
+    main_content.first.css("u").text
+  end
+
+  def events
+    rest = main_content.css("p").drop(1)
+    rest.map do |event|
+        { description: description(event),
+          link: event_link(event)
         }
-      end
     end
   end
+
+  private
+
+    def description(event)
+      event.css("span").text
+    end
+
+    def event_link(event)
+      event.css("a").present? ? event.css("a").attr("href").value : ""
+    end
+
 end
